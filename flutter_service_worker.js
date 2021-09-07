32a5216bcaaee3800455ce349ba650a285aa7b92
MinhoHa <ksky216@naver.com>
2021-09-07 13:46:28 +0900

    갱신

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,23 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+  "assets/AssetManifest.json": "b93443d62eb31e15b2daf262fcdf0106",
+"assets/assets/images/kakaotalk_logo_icon.svg": "8635f7c232a347ac8d0b85ba80b75bfe",
+"assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "fdb932d0c5b4b9b38df7746d616e3446",
+"assets/NOTICES": "3e2f7ebf54591213dd0bd1fb9a8940fc",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3241d1d9c15448a4da96df05f3292ffe",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "eaed33dc9678381a55cb5c13edaf241d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "ffed6899ceb84c60a1efa51c809a57e4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "44168a2e1cc1d6179c82ba171d6626a6",
-"/": "44168a2e1cc1d6179c82ba171d6626a6",
-"main.dart.js": "939789df76c2072526dbfe51747477c8",
-"manifest.json": "10e31171f090afb7f52394ba90b2034e",
-"version.json": "3325e920167865ff96db05ec8889fe52"
+"index.html": "47e5a8199dd69358f588115740073088",
+"/": "47e5a8199dd69358f588115740073088",
+"main.dart.js": "d55647040afb36fd631e5e93e6970d13",
+"manifest.json": "d5aa3f0419fc987b8d5321cc9f09b514",
+"version.json": "8865f0ad695536d7a8785602572beddd"
 };
 
 // The application shell files that are downloaded before a service worker can
