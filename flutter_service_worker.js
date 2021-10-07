6101091c6e531af3fb1858fa602ed68894d34827
MinhoHa <ksky216@naver.com>
2021-10-07 13:41:15 +0900

    갱신

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "b93443d62eb31e15b2daf262fcdf0106",
+  "assets/AssetManifest.json": "ddcd164a3f3cd862bc73d1b6c96d70fa",
+"assets/assets/images/default-image.png": "8f7f581ce5768437106881b8416d410e",
 "assets/assets/images/kakaotalk_logo_icon.svg": "8635f7c232a347ac8d0b85ba80b75bfe",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "2840998bdaed11176f7f3222a9b5e3bd",
+"assets/NOTICES": "19db56db1227a2061f444fa656a093d9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3241d1d9c15448a4da96df05f3292ffe",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "eaed33dc9678381a55cb5c13edaf241d",
@@ -17,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2334f1a951cedbf10d5c9c63a9cb4f27",
-"/": "2334f1a951cedbf10d5c9c63a9cb4f27",
-"main.dart.js": "78880eb83503554763a26f18a89e979e",
+"index.html": "0e15277711713994e14d61be68b9624f",
+"/": "0e15277711713994e14d61be68b9624f",
+"main.dart.js": "290e6a030ad8366218c22962b8229c1f",
 "manifest.json": "d5aa3f0419fc987b8d5321cc9f09b514",
 "version.json": "8865f0ad695536d7a8785602572beddd"
 };
